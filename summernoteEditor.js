a224bb862f596271f81471a24a0808cefb823f5f
dew9163 <dew9163.flybook@gmail.com>
2020-12-17 12:39:43 +0900

    update summernote editor

--- summernoteEditor.js
+++ summernoteEditor.js
@@ -18,8 +18,33 @@ const summernoteEditor = {
         callbacks: undefined,
     },
 
-    init: function () {
-        $('.' + this.props.class).summernote(this.attributes);
+    init: function (options) {
+        console.log(Object.assign(this.attributes, options));
+        $('.' + this.props.class).summernote(Object.assign(this.attributes, options));
+    },
+
+    /**
+     * textarea 에 사용한 정보를 summernote 에 적용 합니다
+     * summernoteEditor.bind('.editor-container','data-class','textarea');
+     * @param selector
+     * summernote 와 textarea 를 포함하는 selector
+     * @param attr
+     * textarea 에서 가져올 attr
+     * @param target
+     * textarea selector
+     * @author  dew9163
+     * @added   2020/12/17
+     * @updated 2020/12/17
+     */
+    bind : function(selector, attr, target) {
+        let selectors = $(selector);
+        for(let i=0;i<selectors.length;i++) {
+            let container = selectors[i];
+            let area = container.querySelector('.note-editing-area');
+            let textarea = container.querySelector(target);
+            let dataClass = $(textarea).attr(attr);
+            $(area.querySelector('.note-editable')).addClass(dataClass)
+        }
     },
 
     getOnImageUploadCallback: function (files, editor, welEditable) {
